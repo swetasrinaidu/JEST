d3e434d4d7431a6ec55305fa252b7ecf96e34bf6
Sweta <srialwarnaidu@gmail.com>
2022-07-19 18:00:58 +0530

    first unit test

---
+++ script.js
@@ -0,0 +1,17 @@
+const googleDataBase = [
+    'cats.com',
+    'soup.com',
+    'flowers.com',
+    'animals.com',
+    'catpictures.com',
+    'myfavouritecats.com'
+];
+const googleSearch =(searchInput,db)=>{
+    const matches = db.filter(website=>{
+        return website.includes(searchInput);
+    })
+    return matches.length > 3 ? matches.slice(0,3):matches;
+}
+console.log(googleSearch('cats',googleDataBase));
+
+module.exports=googleSearch;
